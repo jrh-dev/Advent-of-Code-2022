0d7b8516c64ec2c6f5b92035cf560a21b254f055
James Hardy <jrh-dev@protonmail.com>
2022-12-01 13:50:05 +0000

    tidy up r code line spacings

--- R/d01.r
+++ R/d01.r
@@ -1,30 +1,19 @@
 #' @export
 d01 = function(input, top) {
-  
   input = ._process(input)
-  
-  return(sum(sort(unlist(input),decreasing = TRUE)[1:top]))
-  
+  return(sum(sort(unlist(input), decreasing = TRUE)[1:top]))
 }
 
 ._process = function(data) {
-  
   data = as.double(data)
-  
   splits = c(which(is.na(data)), length(data))
-  
   res = vector(mode = "list", length = length(splits))
-  
   start = 1
-  
+
   for (ii in seq_len(length(splits))) {
-    
     res[ii] = sum(data[start:(splits[ii] - 1)])
-    
     start = splits[ii] + 1
-    
   }
-  
+
   return(res)
 }
-
