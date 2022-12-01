fd427e4c9543035fd85ef55631e9a138c8a82c3c
James Hardy <jrh-dev@protonmail.com>
2022-12-01 12:33:53 +0000

    functionalise python code

--- Python/d01.py
+++ Python/d01.py
@@ -4,24 +4,23 @@ input = f.read().split("\n")
 
 f.close()
 
-input = [int(x) if x not in '' else None for x in input]
+def solve(input,top):
+    input = [int(x) if x not in '' else None for x in input]
+    splits = [i for i, e in enumerate(input) if e == None]
+    splits.append(len(input))
+    res = []
+    start = 0
 
-splits = [i for i, e in enumerate(input) if e == None]
+    for ii in range(len(splits)):
+        res.append(sum(input[start:splits[ii]]))
+        start = splits[ii] + 1  
 
-splits.append(len(input))
+    res.sort(reverse=True)
 
-res = []
-
-start = 0
-
-for ii in range(len(splits)):
-    res.append(sum(input[start:splits[ii]]))
-    start = splits[ii] + 1
-  
-res.sort(reverse=True)
+    return(sum(res[0:top]))
 
 # Part 1 Answer
-print(sum(res[0:1]))
+print(solve(input,1))
 
 # Part 2 Answer
-print(sum(res[0:3]))
+print(solve(input,3))
