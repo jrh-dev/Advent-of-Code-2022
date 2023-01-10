3fcbfedbea2279bd04055b6bdfb42dbce0894cfc
jrh <jrh-dev@protonmail.com>
2023-01-10 22:10:56 +0000

    d17 p1 in R actually working

--- R/d17.r
+++ R/d17.r
@@ -1,35 +1,6 @@
-
-jet = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-
-jet = unlist(strsplit(jet, ""))
-
-#' idea is to track state of each column
-
-
-#' initial matrix
-state = matrix(0, nrow = 1, ncol = 7)
-
-
-####
-
-.#.
-###
-.#.
-
-..#
-..#
-###
-
-#
-#
-#
-#
-
-##
-##
+pob = 2022
 
 box::use(R6)
-
 
 jet_state <- R6$R6Class("jet_state", list(
   idx = 0,
@@ -46,66 +17,71 @@ jet_state <- R6$R6Class("jet_state", list(
   })
 )
 
+jet = readLines("data/d17.txt")
+jet = unlist(strsplit(jet, ""))
+state = NULL
 
+rocks = list(
+  list(c(1,1,1,1), c(3,4,5,6)),
+  list(c(1,2,2,2,3), c(4,3,4,5,4)),
+  list(c(1,2,3,3,3), c(5,5,3,4,5)),
+  list(c(1,2,3,4), c(3,3,3,3)),
+  list(c(1,1,2,2), c(3,4,3,4))
+)
 
+state = matrix(1, nrow = 1, ncol = 7)
 
+js = jet_state$new(jet)
 
-drop = function(x, y, state) {
+drop = function(rock, state) {
   
+  y = rock[[1]]
+  x = rock[[2]]
   
-  if (all(state == 0)) {
-    state = rbind(matrix(0, nrow = (2 + length(unique(y))), ncol = 7), state)
-  } else {
-    state = rbind(matrix(0, nrow = (3 + length(unique(y))), ncol = 7), state)
-  }
-  
-  
+  state = rbind(matrix(0, nrow = length(unique(y)) + 3, ncol = 7), state)
   
   run = TRUE
+  vert_block = 0
   
   while (run) {
     js$iterate(1)
     
-    if (js$dir == "<" & min(x) > 1) {
+    if (js$dir == ">" & max(x) < 7) {
+      try_x = x + 1
+      if (all(sapply(seq_len(length(x)), function(xe) state[y[xe], try_x[xe]]) == 0)) x = try_x
+    } else if (js$dir == "<" & min(x) > 1) {
       try_x = x - 1
-    } else if (js$dir == ">" & max(x) < 7) {
-      try_x = x + 1 
-    }
-    
-    if (!any(sapply(seq_len(length(x)), \(idx) state[y[idx], try_x[idx]] > 0))) {
-      x = try_x
+      if (all(sapply(seq_len(length(x)), function(xe) state[y[xe], try_x[xe]]) == 0)) x = try_x
     }
     
     try_y = y + 1
-    
-    if (any(try_y > nrow(state))) {
-      for (e in seq_len(length(x))) state[y[e],x[e]] = 1
-      run = FALSE
-    } else if (!any(sapply(seq_len(length(x)), \(idx) state[try_y[idx], x[idx]] > 0))) {
+    if (all(sapply(seq_len(length(y)), function(ye) state[try_y[ye], x[ye]]) == 0)) {
       y = try_y
-    } else {
-      for (e in seq_len(length(x))) state[y[e],x[e]] = 1
-      run = FALSE
+      vert_block = 0
+      } else {
+        vert_block = vert_block + 1
     }
+    
+    if (vert_block > 0) run = FALSE
+    
+    ts = state
+    for (e in seq_len(length(x))) ts[y[e], x[e]] = 1
+    ts
   }
-  state = state[which(rowSums(state) > 0),]
+  
+  for (e in seq_len(length(x))) state[y[e], x[e]] = 1
+  
+  state = state[rowSums(state) > 0,]
+  
   return(state)
 }
 
 
-js = jet_state$new(jet)
+for (iter in c(rep(1:5, abs(pob / 5)), 1:(pob%%5))) {
+  
+  state = drop(rock=rocks[[iter]], state)
+  
+}
 
 
-state = matrix(0, nrow = 1, ncol = 7)
-
-state = drop(x=c(3,4,5,6), y=c(1,1,1,1), state=state)
-
-state
-
-
-state = drop(x=c(3,4,4,4,5), y=c(2,2,1,3,2), state=state)
-
-
-
-
-
+nrow(state) - 1
