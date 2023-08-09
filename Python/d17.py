eced60db7995583dc905ed75bbc9f2fce3594e01
jrh <jrh-dev@protonmail.com>
2023-08-09 08:08:48 +0100

    python d17 p1

--- Python/d17.py
+++ Python/d17.py
@@ -1,3 +1,73 @@
+def req_rows(shape: list[int], placed: list[int]) -> int:
+    new = max(set([x for x, _ in shape])) + 3
+    if len(placed) != 0:
+        exist = (
+            max(set([x for x, _ in placed])) - min(set([x for x, _ in placed]))
+        ) + 1
+    else:
+        exist = 0
+    return new + exist
+
+
+def can_move(shp_pos, placed, dir, grid_rows):
+    if dir == ">":
+        new_pos = [(r, c + 1) for r, c in shp_pos]
+    elif dir == "<":
+        new_pos = [(r, c - 1) for r, c in shp_pos]
+    elif dir == "v":
+        new_pos = [(r + 1, c) for r, c in shp_pos]
+
+    for r, c in new_pos:
+        if (r, c) in placed or r > grid_rows or c < 0 or c > 6:
+            return (shp_pos, False)
+        else:
+            continue
+
+    return (new_pos, True)
+
+
+def tower_height(shapes: list[list[int]], jets: str, n_drops) -> int:
+    i = 0
+    j = 0
+    dropped = 0
+    placed = []
+    shp_pos = shapes[j % len(shapes)]
+    grid_rows = req_rows(shp_pos, placed)
+    states = []
+
+    while dropped < n_drops:
+        jet = jets[i % len(jets)]
+
+        if jet == ">":
+            shp_pos, _ = can_move(shp_pos, placed, ">", grid_rows)
+            shp_pos, moved = can_move(shp_pos, placed, "v", grid_rows)
+        elif jet == "<":
+            shp_pos, _ = can_move(shp_pos, placed, "<", grid_rows)
+            shp_pos, moved = can_move(shp_pos, placed, "v", grid_rows)
+
+        if moved:
+            i += 1
+        else:
+            i += 1
+            j += 1
+            dropped += 1
+            for pos in shp_pos:
+                placed.append(pos)
+            shp_pos = shapes[j % len(shapes)]
+            grid_rows = req_rows(shp_pos, placed)
+            drop_by = grid_rows - max(set([x for x, _ in placed]))
+            for pos in range(len(placed)):
+                placed[pos] = (placed[pos][0] + drop_by, placed[pos][1])
+
+            "".join([f"{r}{c}" for r, c in placed if r <= 10])
+
+            states.append((max(set([x for x, _ in placed])) - min(set([x for x, _ in placed]))) + 1)
+
+    height = (max(set([x for x, _ in placed])) - min(set([x for x, _ in placed]))) + 1
+
+    return (height, states)
+
+
 # shapes
 shapes = [
     [(0, 2), (0, 3), (0, 4), (0, 5)],
@@ -7,67 +77,10 @@ shapes = [
     [(0, 2), (0, 3), (1, 2), (1, 3)],
 ]
 
-
-# class for shapes
-class Shape:
-    """
-    A class to represent a 2d shape by recording each
-    of its individual coordinates.
-    """
-
-    def __init__(self, shape: list[set[int]]):
-        self.shape = shape
-
-    def __repr__(self) -> str:
-        return f"Shape({self.shape})"
-
-    def req_rows(self) -> int:
-        """
-        Calculate how many rows are required to fit the shape.
-        """
-        return len(set([x for x, _ in self.shape])) + 3
-
-
 jets = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
+with open("data/d17.txt", "r") as file:
+    jets = file.read()
 
-# move the shapes left
-def move_left(shape: list[set[int]]) -> list[set[int]]:
-    shape = []
-    for point in shape:
-        x, y = point
-        shape.append((x - 1, y))
-    return shapes
-
-
-# move the shapes right
-def move_right(shape: list[set[int]]) -> list[set[int]]:
-    shape = []
-    for point in shape:
-        x, y = point
-        shape.append((x + 1, y))
-    return shapes
-
-
-# move the shapes down
-def move_down(shape: list[set[int]]) -> list[set[int]]:
-    shape = []
-    for point in shape:
-        x, y = point
-        shape.append((x, y + 1))
-    return shapes
-
-
-grid = [[0, 0, 1, 0, 0, 0, 0]]
-
-test_shape = [(2, 0)]
-
-
-def can_move(shape: list[set[int]], grid: list[list[int]]) -> bool:
-    for point in shape:
-        x, y = point
-        if grid[y][x] == 1:
-            return False
-    return True
-
+height, pattern = tower_height(shapes, jets, 2022)
 
